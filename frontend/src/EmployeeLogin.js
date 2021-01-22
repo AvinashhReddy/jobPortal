6cf9b0dffd0d25b634070eacf29b39fb658970c3
AvinashhReddy <avinash12797@gmail.com>
2021-01-22 19:34:17 +0530

    commit

--- frontend/src/EmployeeLogin.js
+++ frontend/src/EmployeeLogin.js
@@ -1,6 +1,28 @@
+import {Form,Button} from 'react-bootstrap'
+
 function EmployeeLogin(){
     return (<div>
-        In Employee
+        <div style={{padding:'4vw',paddingRight:'70vw',
+}}>
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Enter OTP sent to the above email address</Form.Label>
+    <Form.Control  placeholder="OTP" />
+  </Form.Group>
+ 
+  <Button variant="primary" type="submit">
+    Login
+  </Button>
+</Form>
+</div>
     </div>)
 }
 
--- frontend/src/UseLogin.js
+++ frontend/src/UseLogin.js
@@ -1,6 +1,27 @@
+
+import {Form,Button} from 'react-bootstrap'
 function Userlogin(){
-    return (<div>
-        In userlogin
+      return (<div>
+        <div style={{padding:'4vw',paddingRight:'70vw'}}>
+        <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Enter OTP sent to the above email address</Form.Label>
+    <Form.Control  placeholder="OTP" />
+  </Form.Group>
+ 
+  <Button variant="primary" type="submit">
+    Login
+  </Button>
+</Form>
+</div>
     </div>)
 }
 export default Userlogin;
