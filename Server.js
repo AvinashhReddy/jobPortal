8d59c8d7f7b71dd75dfc68e73258359b5a635f71
AvinashhReddy <avinash12797@gmail.com>
2021-01-26 10:17:32 +0530

    v

--- Server.js
+++ Server.js
@@ -73,7 +73,5 @@ app.post('/addJob',(req,res)=>{
     // Set static folder
     app.use(express.static('client/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    
   }
